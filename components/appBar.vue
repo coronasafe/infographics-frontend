1da647e040ecb56dec725c100136b0d5fde9af90
vighnesh <vighneshaka8921@gmail.com>
2020-03-21 12:22:53 +0530

    ui changes on the appbar

--- components/appBar.vue
+++ components/appBar.vue
@@ -1,22 +1,27 @@
 <template>
   <div>
-    <v-app-bar dark >
-      <v-toolbar-title>Corona Infographics</v-toolbar-title>
-      <v-spacer></v-spacer>
+    <v-app-bar dark color="blue">
+      <v-container class="d-flex">
+        
+        <v-toolbar-title>Corona Infographics</v-toolbar-title>
+              <v-spacer></v-spacer>
 
-      <div>
-        <v-menu open-on-hover top offset-y>
-          <template v-slot:activator="{ on }">
-            <v-btn dark v-on="on">Language</v-btn>
-          </template>
+              <div>
+                <v-menu open-on-hover top offset-y >
+                  <template v-slot:activator="{ on }">
+                    <v-btn dark v-on="on" color = "blue darken-2">Language</v-btn>
+                  </template>
 
-          <v-list>
-            <v-list-item v-for="(item, index) in items" :key="index" >
-              <v-list-item-title>{{ item }}</v-list-item-title>
-            </v-list-item>
-          </v-list>
-        </v-menu>
-      </div>
+                  <v-list>
+                    <v-list-item v-for="(item, index) in items" :key="index" >
+                      <v-list-item-title>{{ item }}</v-list-item-title>
+                    </v-list-item>
+                  </v-list>
+                </v-menu>
+              </div>
+
+      </v-container>
+      
     </v-app-bar>
   </div>
 </template>
